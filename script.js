b1d6b84e121075a51f75b050b477dbdc1de547c1
Prof. Marcelo Júnior <113000269+MadlyJr@users.noreply.github.com>
2025-08-29 00:51:20 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-  <script>
-    const hour = new Date().getHours();
-    if(hour >= 18 || hour < 6){document.body.classList.add('dark');}
+const hour = new Date().getHours();
+if(hour >= 18 || hour < 6){document.body.classList.add('dark');}
 
-    document.querySelectorAll('.card').forEach(card=>{
-      card.addEventListener('click',()=>{
-        card.classList.toggle('active');
-      });
-    });
-  </script>
+document.querySelectorAll('.card').forEach(card=>{
+  card.addEventListener('click',()=>{
+    card.classList.toggle('active');
+  });
+});
