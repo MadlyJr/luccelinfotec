829c31a3dc0ea4d0410d0a8ffe0df598e3219f9a
Prof. Marcelo Júnior <113000269+MadlyJr@users.noreply.github.com>
2025-08-29 00:24:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+  <script>
+    const hour = new Date().getHours();
+    if(hour >= 18 || hour < 6){document.body.classList.add('dark');}
+
+    document.querySelectorAll('.card').forEach(card=>{
+      card.addEventListener('click',()=>{
+        card.classList.toggle('active');
+      });
+    });
+  </script>
